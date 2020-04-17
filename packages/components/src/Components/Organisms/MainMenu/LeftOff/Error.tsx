cda8ba0f2ec78968177069fb96a7f5b5e0316795
Diogo Biz <diogo@codengage.com>
2020-04-17 10:08:32 -0300

    resolve imports

--- packages/components/src/Components/Organisms/MainMenu/LeftOff/Error.tsx
+++ packages/components/src/Components/Organisms/MainMenu/LeftOff/Error.tsx
@@ -1,31 +1,31 @@
-import React from 'react'
-import styled from 'styled-components'
-import { theme } from 'styled-tools'
-import { SANTypography } from '../../../Atoms/Typography'
+import React from "react";
+import styled from "styled-components";
+import { theme } from "styled-tools";
+import { SANTypography } from "../../../Atoms/Typography";
 
-import error from 'Assets/images/error/load.svg'
-import { useTranslation } from 'react-i18next'
-import SANLeftOffContainer from './Container'
+import error from "../../../../Assets/images/error/load.svg";
+import { useTranslation } from "react-i18next";
+import SANLeftOffContainer from "./Container";
 
 const SANLeftOffErrorStyled: React.FC = styled(SANLeftOffContainer)`
-    color: ${theme('colors.white.10')};
-    text-align: center;
+  color: ${theme("colors.white.10")};
+  text-align: center;
 
-    && img {
-        margin-bottom: ${theme('space.7')};
-    }
-`
+  && img {
+    margin-bottom: ${theme("space.7")};
+  }
+`;
 
 const SANLeftOffError = () => {
-    const { t } = useTranslation('components')
-    return (
-        <SANLeftOffErrorStyled>
-            <img src={error} alt='error-on-load' />
-            <SANTypography variant='overline' transform='initial'>
-                {t('error.leftOff')}
-            </SANTypography>
-        </SANLeftOffErrorStyled>
-    )
-}
+  const { t } = useTranslation("components");
+  return (
+    <SANLeftOffErrorStyled>
+      <img src={error} alt="error-on-load" />
+      <SANTypography variant="overline" transform="initial">
+        {t("error.leftOff")}
+      </SANTypography>
+    </SANLeftOffErrorStyled>
+  );
+};
 
-export default SANLeftOffError
+export default SANLeftOffError;
--- packages/components/src/Components/Organisms/MainMenu/MainMenu.stories.tsx
+++ packages/components/src/Components/Organisms/MainMenu/MainMenu.stories.tsx
@@ -1,86 +1,86 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-import { text, number, select } from '@storybook/addon-knobs'
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { text, number, select } from "@storybook/addon-knobs";
 
-import { SANLeftOff } from './LeftOff'
+import { SANLeftOff } from "./LeftOff";
 
 // Images
-import questionImage from 'Assets/images/course-items/question.svg'
-import mentalmapImage from 'Assets/images/course-items/mental-map.svg'
-import flowchartImage from 'Assets/images/course-items/flow.svg'
-import articleImage from 'Assets/images/course-items/article.svg'
-import documentImage from 'Assets/images/course-items/document.svg'
+import questionImage from "../../../Assets/images/course-items/question.svg";
+import mentalmapImage from "../../../Assets/images/course-items/mental-map.svg";
+import flowchartImage from "../../../Assets/images/course-items/flow.svg";
+import articleImage from "../../../Assets/images/course-items/article.svg";
+import documentImage from "../../../Assets/images/course-items/document.svg";
 
 const LeftOffExample = ({ resourceType, type }) => {
-    const configureThumbnail = (type, resourceType) => {
-        switch (resourceType) {
-            case 'Video':
-                return 'https://cdn.jwplayer.com/thumbs/eqXRiD4T-720.jpg'
-            case 'Question':
-                return questionImage
-            case 'Document':
-                switch (type) {
-                    case 'mentalmap':
-                        return mentalmapImage
-                    case 'flowchart':
-                        return flowchartImage
-                    case 'article':
-                        return articleImage
-                    default:
-                        return documentImage
-                }
-            default:
-                return documentImage
+  const configureThumbnail = (type, resourceType) => {
+    switch (resourceType) {
+      case "Video":
+        return "https://cdn.jwplayer.com/thumbs/eqXRiD4T-720.jpg";
+      case "Question":
+        return questionImage;
+      case "Document":
+        switch (type) {
+          case "mentalmap":
+            return mentalmapImage;
+          case "flowchart":
+            return flowchartImage;
+          case "article":
+            return articleImage;
+          default:
+            return documentImage;
         }
+      default:
+        return documentImage;
     }
+  };
 
-    return (
-        <div style={{ width: 320 }}>
-            <SANLeftOff
-                label={text('Label', 'This is a label')}
-                onClick={console.log}
-                title={text('Title', 'This is a title')}
-                resourceType={resourceType}
-                thumbnail={configureThumbnail(type, resourceType)}
-                classReference={text('Class Reference', 'Class 1')}
-                moduleReference={text('Module Reference', 'Module 1')}
-            />
-        </div>
-    )
-}
+  return (
+    <div style={{ width: 320 }}>
+      <SANLeftOff
+        label={text("Label", "This is a label")}
+        onClick={console.log}
+        title={text("Title", "This is a title")}
+        resourceType={resourceType}
+        thumbnail={configureThumbnail(type, resourceType)}
+        classReference={text("Class Reference", "Class 1")}
+        moduleReference={text("Module Reference", "Module 1")}
+      />
+    </div>
+  );
+};
 
 const resourceTypeOptions: any = {
-    Video: 'Video',
-    Document: 'Document',
-    Question: 'Question'
-}
+  Video: "Video",
+  Document: "Document",
+  Question: "Question"
+};
 
 const typeOptions: any = {
-    Resume: 'resume',
-    Mentalmap: 'mentalmap',
-    Flowchart: 'flowchart',
-    Article: 'article',
-    Lesson: 'lesson',
-    Question: 'question'
-}
+  Resume: "resume",
+  Mentalmap: "mentalmap",
+  Flowchart: "flowchart",
+  Article: "article",
+  Lesson: "lesson",
+  Question: "question"
+};
 
-storiesOf('Organisms.MainMenu', module).add(
-    'LeftOff',
-    () => {
-        return (
-            <LeftOffExample
-                resourceType={select(
-                    'Resource Type',
-                    resourceTypeOptions,
-                    resourceTypeOptions.Question
-                )}
-                type={select('Type', typeOptions, typeOptions.Question)}
-            />
-        )
-    },
-    {
-        style: {
-            background: '#7A2044'
-        }
+storiesOf("Organisms.MainMenu", module).add(
+  "LeftOff",
+  () => {
+    return (
+      <LeftOffExample
+        resourceType={select(
+          "Resource Type",
+          resourceTypeOptions,
+          resourceTypeOptions.Question
+        )}
+        type={select("Type", typeOptions, typeOptions.Question)}
+      />
+    );
+  },
+  {
+    style: {
+      background: "#7A2044"
     }
-)
+  }
+);
